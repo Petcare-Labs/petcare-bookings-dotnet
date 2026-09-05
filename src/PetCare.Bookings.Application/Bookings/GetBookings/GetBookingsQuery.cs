4a81be8c59770b7bd178527b577e76ffccce4cb1
Jake Reck <jakereck@gmail.com>
2026-09-05 12:58:08 -0400

    Fixing

--- src/PetCare.Bookings.Application/Bookings/GetBookings/GetBookingsQuery.cs
+++ src/PetCare.Bookings.Application/Bookings/GetBookings/GetBookingsQuery.cs
@@ -2,4 +2,4 @@ using PetCare.Bookings.Domain.Enums;
 
 namespace PetCare.Bookings.Application.Bookings.GetBookings;
 
-public sealed record GetBookingsQuery(Guid? CustomerId, Guid? ProviderId, BookingStatus? Status, int PageNumber = 1, int pageSize = 20);
+public sealed record GetBookingsQuery(Guid? CustomerId, Guid? ProviderId, BookingStatus? Status, int PageNumber = 1, int PageSize = 20);
